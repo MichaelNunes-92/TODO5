6fcc03b4867c97cc311a46cd73391a6c13d68464
MichaelNunes-92 <mikenunes2019@gmail.com>
2022-09-17 21:12:43 -0300

    Arquivos de SQL

---
+++ DML-Inserts.sql
@@ -0,0 +1,64 @@
+USE LIVROS;
+
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+1, '1984', 'George Orwell', 'ficção', 'Inglaterra', 308, 90.00);
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+2, 'Neuromancer', 'William Gibson', 'ficção', 'Estudos Unidos', 416, 79.90); 
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+3, 'Count Zero', 'William Gibson', 'ficção', 'Estudos Unidos', 311, 59.90); 
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+4, 'Mona Lisa Overdrive', 'William Gibson', 'ficção', 'Estudos Unidos', 320, 65.90);
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+5, 'Fundação', 'Isaac Asimov', 'ficção', 'Russia', 320, 55.90);  
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+6, 'Segunda Fundação', 'Isaac Asimov', 'ficção', 'Russia', 304, 45.90);  
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+7, 'Fundação e Império', 'Isaac Asimov', 'ficção', 'Russia', 328, 75.90);
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+8, 'Eu, Robô', 'Isaac Asimov', 'ficção', 'Russia', 315, 25.90);
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+9, 'Androis sonham com ovelhas elétricas?', 'Philip K. Dick', 'ficção', 'Estados Unidos', 288, 45.90);
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+10, 'Sonhos Elétricos', 'Philip K. Dick', 'ficção', 'Estados Unidos', 248, 32.90);
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+11, 'O homem do Castelo Alto', 'Philip K. Dick', 'ficção', 'Estados Unidos', 239, 55.90);
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+12, 'A Guerra dos Tronos', 'George R.R. Martin', 'ficção', 'Estados Unidos', 592, 45.90);
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+13, 'A Fúria dos Reis', 'George R.R. Martin', 'ficção', 'Estados Unidos', 648, 55.90);
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+14, 'A Tormenta de Espadas', 'George R.R. Martin', 'ficção', 'Estados Unidos', 848, 65.90);
+INSERT INTO PRODUTOS (
+ID, NOME, AUTOR, GENERO, NACIONALIDADE, PAGINAS, PRECO)
+	VALUES (
+15, 'O Festim de Corvos', 'George R.R. Martin', 'ficção', 'Estados Unidos', 384, 35.90);
+
+
